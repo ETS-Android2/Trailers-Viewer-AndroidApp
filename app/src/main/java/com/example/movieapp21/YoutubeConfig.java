0dbd2e4f451d8b6e65bea4635e1d45e80aa51381
Erges Mema <77251761+ergesmema@users.noreply.github.com>
2021-09-26 13:51:59 +0200

    Update YoutubeConfig.java

--- app/src/main/java/com/example/movieapp21/YoutubeConfig.java
+++ app/src/main/java/com/example/movieapp21/YoutubeConfig.java
@@ -4,7 +4,7 @@ public class YoutubeConfig {
     public YoutubeConfig(){
 
     }
-    private static final String API_KEY = "AIzaSyCBqjkyFf4LKj6Si5lsYdBLMy2bBSaqkfg";
+    private static final String API_KEY = "";
 
     public static String getApiKey(){
         return API_KEY;
